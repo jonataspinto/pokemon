671c48282cde643b832b51ceb3561f42665de084
jonataspinto <jonatastdb_jp@hotmail.com>
2020-02-29 01:03:44 -0300

    feat: implementa eslint

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,5 @@
-import React from 'react'
+import React from 'react';
 
-const Home = () =>{
-    return (
-        <div>
-            home works
-        </div>
-    )
-}
+const Home = () => <div>home works</div>;
 
-export default Home
+export default Home;
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,8 @@
 /* PLOP_INJECT_IMPORT */
-import {Home} from './Home'
+import Home from './Home';
 
 export {
-    /* PLOP_INJECT_EXPORT */
-    Home
-  }
+  /* PLOP_INJECT_EXPORT */
+  // eslint-disable-next-line import/prefer-default-export
+  Home,
+};
